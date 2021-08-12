07e2a81087e31a3cd8fa7d9b94b33c3016397d25
Yoshi3lines <ny3line@gmail.com>
2021-08-13 01:15:55 +0900

    showページを編集

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2021_08_12_134347) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-08-11 23:00:00"
-    t.datetime "work_time", default: "2021-08-11 22:30:00"
+    t.datetime "basic_time", default: "2021-08-12 23:00:00"
+    t.datetime "work_time", default: "2021-08-12 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
